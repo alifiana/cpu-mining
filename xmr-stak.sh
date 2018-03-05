ee1c432968b909265e57bcdf6424f5e447dc3dfe
alfianghz <36778850+alfianghz@users.noreply.github.com>
2018-03-05 15:21:02 +0700

    Update xmr-stak.sh

--- xmr-stak.sh
+++ xmr-stak.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-apt-get install software-properties-common
+apt-get -y install software-properties-common
 add-apt-repository ppa:ubuntu-toolchain-r/test 
-apt-get update 
+apt-get -y update 
 apt-get install gcc-5 g++-5 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 
 apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev 
-apt-get install git 
+apt-get -y install git 
 git clone "https://github.com/fireice-uk/xmr-stak.git" 
 mkdir xmr-stak/build 
 cd xmr-stak/build 
