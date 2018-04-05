1606d7ea5db67f2d2b4cebd89277be7c8625fec1
alifiana <caricara1001@gmail.com>
2018-04-05 14:46:30 +0700

    Update xmr-stak.sh

--- xmr-stak.sh
+++ xmr-stak.sh
@@ -6,7 +6,7 @@ apt-get install gcc-5 g++-5
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 
 apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev 
 apt-get install git 
-git clone "https://github.com/stellitequestion/xmr-stak.git" 
+git clone "https://github.com/AltcoinCoop/xmr-stak-haven.git" 
 mkdir xmr-stak/build 
 cd xmr-stak/build 
 cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF .. 
